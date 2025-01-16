53c4864a72c0fddbf5cf5f58d6834baf87a27ee5
Tushar Khushal Mahajan <mahajantushar208@gmail.com>
2025-01-16 16:38:26 +0530

    added actuator and resilience4j properties & add circuit breaker fallback method

--- UserEntity/src/main/java/com/userentity/controllers/UserController.java
+++ UserEntity/src/main/java/com/userentity/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.userentity.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import com.userentity.entities.User;
 import com.userentity.services.UserServiceImpl;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -20,33 +23,47 @@ import lombok.NoArgsConstructor;
 @RequestMapping("/user")
 public class UserController {
 
-  @Autowired
-  private UserServiceImpl serviceImpl;
+	@Autowired
+	private UserServiceImpl serviceImpl;
 
-  @GetMapping
-  public ResponseEntity<List<User>> getAllUsers() {
-    List<User> users = serviceImpl.getAllUsers();
+	Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    System.out.println("All users");
+	@GetMapping
+	public ResponseEntity<List<User>> getAllUsers() {
+		List<User> users = serviceImpl.getAllUsers();
 
-    return ResponseEntity.ok().body(users);
-  }
+		System.out.println("All users");
 
-  @GetMapping("/{userId}")
-  public ResponseEntity<User> getUserById(@PathVariable String userId) {
+		return ResponseEntity.ok().body(users);
+	}
 
-    User user = serviceImpl.getUserById(userId);
+	@GetMapping("/{userId}")
+	@CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallBack")
+	public ResponseEntity<User> getUserById(@PathVariable String userId) {
 
-    return ResponseEntity.status(HttpStatus.FOUND).body(user);
-  }
+		User user = serviceImpl.getUserById(userId);
 
-  @PostMapping
-  public ResponseEntity<User> insertUser(@RequestBody User user) {
+		return ResponseEntity.status(HttpStatus.FOUND).body(user);
+	}
 
-    System.out.println("User insert controller");
+//	ratingHotelFallBack method for circuit breaker
+	public ResponseEntity<User> ratingHotelFallBack(String userId, Exception ex) {
 
-    User insertUser = serviceImpl.InsertUser(user);
+		logger.info("FALLBACK IS EXECUTED BECAUSE SERVICE IS DOWN", ex.getMessage());
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(insertUser);
-  }
+		User user = new User("userId", "Dummy User", "dummy@gmail.com", null);
+
+		return ResponseEntity.ok(user);
+	}
+
+//	----------------------------------
+	@PostMapping
+	public ResponseEntity<User> insertUser(@RequestBody User user) {
+
+		System.out.println("User insert controller");
+
+		User insertUser = serviceImpl.InsertUser(user);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(insertUser);
+	}
 }
